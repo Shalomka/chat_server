256e231721b5ee365ea1d1e05a544ef37ee88e74
Robertas Skiauteris <Shalomka@users.noreply.github.com>
2024-06-17 11:06:56 +0300

    added message handling and streams

--- lib/src/features/chat/chat_service.dart
+++ lib/src/features/chat/chat_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:chat_proto/chat_proto.dart';
@@ -5,9 +6,14 @@ import 'package:uuid/uuid.dart';
 
 class ChatService extends ChatServiceBase {
   final Store store;
-  final ChannelCacheRepo repo;
+  final ChannelCacheRepo chatRepo;
+  final MessageCacheRepo messageRepo;
+  final StreamController<MessageResponse> _messageStreamController =
+      StreamController<MessageResponse>.broadcast();
 
-  ChatService(this.store) : repo = ChannelCacheRepo(store);
+  ChatService(this.store)
+      : chatRepo = ChannelCacheRepo(store),
+        messageRepo = MessageCacheRepo(store);
 
   @override
   Future<CreateChannelResponse> createChannel(
@@ -23,14 +29,14 @@ class ChatService extends ChatServiceBase {
       timestamp,
     );
 
-    await repo.put(cachedChannel);
+    await chatRepo.put(cachedChannel);
     return CreateChannelResponse();
   }
 
   @override
   Future<ListChannelsResponse> listChannels(
       ServiceCall call, ListChannelsRequest request) async {
-    final channels = await repo.getAll();
+    final channels = await chatRepo.getAll();
     final response = ListChannelsResponse();
     response.channels.addAll(channels.map((e) => Channel.fromBuffer(e.data)));
     return response;
@@ -39,14 +45,24 @@ class ChatService extends ChatServiceBase {
   @override
   Future<SendMessageResponse> sendMessage(
       ServiceCall call, SendMessageRequest request) {
-    // TODO: implement sendMessage
-    throw UnimplementedError();
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    final message = MessageResponse()
+      ..messageId = timestamp
+      ..channelId = request.channelId
+      ..text = request.text
+      ..timestamp = Timestamp();
+    final Uint8List data = message.writeToBuffer();
+    final cachedMessage = CachedMessage(request.channelId, data, timestamp);
+    messageRepo.put(cachedMessage);
+    _messageStreamController.add(message);
+    final response = SendMessageResponse()..channelId = request.channelId;
+    return Future.value(response);
   }
 
   @override
   Stream<MessageResponse> streamMessages(
       ServiceCall call, StreamMessagesRequest request) {
-    // TODO: implement streamMessages
-    throw UnimplementedError();
+    return _messageStreamController.stream
+        .where((message) => message.channelId == request.channelId);
   }
 }
